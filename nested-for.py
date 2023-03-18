e2c41ae90c8ca22e3f6e6d4950a3cac06a6bfb43
“ijasz” <jass07rtr@gmail.com>
2023-03-18 18:25:49 +0530

    nested loop and while loop

---
+++ nested-for.py
@@ -0,0 +1,37 @@
+##for i in range(2):
+##    for j in range(2):
+##        for k in range(2):
+##            print(i,j,k)
+    
+
+##print("hello", end="\n")
+##print("hello", end="")
+##print("hi")
+##print("hi")
+
+##row = 5
+##col = 4
+
+##for i in range(row):
+##    for j in range(col):
+##        print("*", end=" ")
+##    print()
+
+##for i in range(5):
+##    for j in range(i + 1):
+##        print("*", end=" ")
+##    print()
+
+
+##for i in range(row):
+##    print("* " * col)
+
+##for i in range(6):
+##    print("* " * i)
+
+##a = 0
+##
+##while(a < 20):
+##    a += 1
+##    print(a)
+    
