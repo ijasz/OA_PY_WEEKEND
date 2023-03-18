066ce80378fcf773e88bacd15df4b879cf0f6c6a
“ijasz” <jass07rtr@gmail.com>
2023-03-18 12:48:19 +0530

    for loop

---
+++ forloop.py
@@ -0,0 +1,123 @@
+##for i in range(11, 5, -2):
+##    print(i)
+
+##print(i)
+
+#String Slicing
+
+##a = "hello"
+
+##print("j" + a[1:])
+
+##print(a[4:1:-1])
+
+##for i in a:
+##    print(i)
+
+##PALINDROME
+##a=input("Enter the value :")
+##if(a[0::1]==a[::-1]):
+##    print("The entered value is a palindrome")
+##else:
+##    print("The entered value is not a palindrome")
+
+##for i in range(a[0::1]==a[::-1]):
+##    print("The entered value is a palindrome")
+##    print(a[::-1])
+##    break
+##else:
+##    print("The entered value is not a palindrome")
+##    print(a[::-1])
+
+##FIRST 10 NATURAL NO:s
+##for i in range(1, 11):
+##    print(i)
+
+##SUM OF NATURAL NO:S
+##for i in range(1, 11):
+##    print(i)
+##print("The sum is: ",sum(range(1,11)))
+
+##N TERMS OF NATURAL NO:S AND THEIR SUM
+##a=int(input("Enter the value :"))
+##for a in range(1, a):
+##      print(a)
+##print("The sum is: ",sum(range(1, 1+a)))
+
+##TO READ 2 NUMBERS FROM KEYBOARD AND FIND THIER SUM
+##AND AVERAGE
+##a=int(input("Number 1: ")), int(input("Number 2: "))
+##print(sum(a))
+##print(sum(a)/2)
+
+##CUBE OF NUMBER
+##a=int(input("Number is: "))
+##print(a**3)
+
+##for i in range(1,6):
+##    i=int(input("Enter number 1: ", [3]))
+##    print(i**3)
+##a = "malayalam"
+##output = ''
+
+##for i in range(len(a)-1, -1, -1):
+##    output += a[i]
+
+##output = a[-1::-1]
+##
+##print(output)
+##if(output == a):
+##    print("Yes")
+##else:
+##    print("no")
+
+
+##print(output)
+##output += "o"
+##print(output)
+##output += "l"
+##print(output)
+##output += "l"
+##print(output)
+##output += "e"
+##print(output)
+##output += "h"
+##print(output)
+
+##user = 4
+##
+##for i in range(user):
+##    input("ddf")
+
+
+table = 5
+time = 2
+
+print(f"{table} * {time} = {table * time}")
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
